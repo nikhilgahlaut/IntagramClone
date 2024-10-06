b3e7c640b140390d7193d1772d11a47f6169b5cc
Nikhil Gahlaut <2015gahlaut.nikhil@gmail.com>
2024-10-06 22:39:56 +0530

    adding nav to home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,30 @@
-function Home() {
-    return (
-        <>
-            <div>Home</div>
-        </>
-    )
-}
-export default Home
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+
+<nav className="navbar">
+  <div className="navbar-left">
+    <a href="/" className="logo">
+      Instagram Clone
+    </a>
+  </div>
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/login">Login</a>
+      </li>
+      <li>
+        <a href="/signup">Sign Up</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+);
+};
+
+export default Home;
